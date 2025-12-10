caed4b746df42b8d936c7bb3262e06ddb1dc99bf
koudo88 <kdorinelaura@gmail.com>
2025-12-10 05:06:57 +0100

    Add inventory management functions for stock updates

--- wareneingang.py
+++ wareneingang.py
@@ -1,1 +1,14 @@
 
+lagerbestand = 50 
+def wareneingang(): 
+global lagerbestand 
+lagerbestand += 10 
+print(f"Neuer Bestand: {lagerbestand}") 
+
+
+def warenausgang(): 
+global lagerbestand 
+lagerbestand -= 5 
+print(f"Neuer Bestand: {lagerbestand}") 
+wareneingang() 
+warenausgang()
