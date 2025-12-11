0b477f75e20eb67ada32b26dcb7fcbb2cf7a169d
koudo88 <kdorinelaura@gmail.com>
2025-12-11 04:36:46 +0100

    Rename function and add mean calculation function
    Renamed the function to 'mittelwer_berechnen' and added a new function 'berechne_mittelwert' with detailed docstring for calculating the arithmetic mean.

--- mittelwert_berechnen.py
+++ mittelwert_berechnen.py
@@ -1,8 +1,31 @@
-def mittelwert_berechnen(* args):
+def mittelwer_berechnen(* args):
   mittel = sum(args)/ len(args)
 return mittel
 
-print(mittelwert_berechnen(1,2,3))
-print(mittelwert_berechnen(10,20,30,40,50))
+print(mittelwer_berechnen(1,2,3))
+print(mittelwer_berechnen(10,20,30,40,50))
+
+def berechne_mittelwert(*werte: int) -> int:
+    """Berechnet den arithmetischen Mittelwert beliebig vieler Zahlen.
+
+    Args:
+        *werte: Eine beliebige Anzahl an Ganzzahlen,deren Durchschnitt ermittelt werden soll.
+
+    Returns:
+        float: Der Durchschnitt aller übergebenen Werte.
+
+    Raises:
+        ValueError: Wenn keine Werte übergeben wurden.
+    """
+    if not werte:
+        raise ValueError("Es muss mindestens ein numerischer Wert übergeben werden.")
+    
+
+    return sum((wert) for wert in werte) / len(werte)
+  
+  print(berechne_mittelwert(1, 2, 3))              
+  print(berechne_mittelwert(10, 20, 30, 40, 50))   
+
+
 
   
